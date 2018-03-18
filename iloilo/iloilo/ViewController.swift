07fbe8c39eb1ea1d18707c0430ac503a9b99c712
Tomooki <tomo3580108@gmail.com>
2018-03-18 20:34:55 +0900

    add テキトーにフォーム作る

--- iloilo/iloilo/ViewController.swift
+++ iloilo/iloilo/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var formTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func didTouchUpConfirmButton(_ sender: UIButton) {
+        resultLabel.text = formTextField.text
+    }
 
 }
 
